3d8bd86a80aca7455e33d05a3efdff4926f05d0d
MelMachine <90393579+MelMachine@users.noreply.github.com>
2022-02-13 16:36:07 +1100

    Create Q2.py

---
+++ QuizSecond/Q2.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        row, col = len(matrix) - 1, 0
+
+        while row >= 0 and col < len(matrix[0]):
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] > target:
+                row -= 1
+            else: 
+                col += 1
+        return False
